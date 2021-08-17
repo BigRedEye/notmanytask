2e1ae37ab5a8da8f9a44b85c1fb274ab632a683e
BigRedEye <mail@bigredeye.me>
2021-08-18 01:14:40 +0300

    feat: Fetch user login from gitlab

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/rakyll/statik v0.1.7
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.8.1
+	github.com/xanzy/go-gitlab v0.50.3
 	go.uber.org/zap v1.19.0
 	golang.org/x/oauth2 v0.0.0-20210402161424-2e8d93401602
 )
---
+++ internal/web/gitlab.go
@@ -0,0 +1,33 @@
+package web
+
+import (
+	"net/http"
+
+	"github.com/pkg/errors"
+	"github.com/xanzy/go-gitlab"
+)
+
+type User struct {
+	ID    int
+	Login string
+}
+
+func GetGitLabUser(token string) (*User, error) {
+	client, err := gitlab.NewOAuthClient(token)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create gitlab client")
+	}
+
+	user, resp, err := client.Users.CurrentUser()
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to get current user")
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("Failed to get current user: %s", resp.Status)
+	}
+
+	return &User{
+		ID:    user.ID,
+		Login: user.Username,
+	}, nil
+}
