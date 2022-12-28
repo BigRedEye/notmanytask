71588da7ec0d0cb53a058a2807be781ce064b1a0
Sergey Skvortsov <mail@bigredeye.me>
2022-12-28 13:57:03 +0300

    feat(fuzz): Add subcommand to run fuzzing process

--- cmd/fuzz/main.go
+++ cmd/fuzz/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
+	"path"
+	"reflect"
+	"sync"
 	"time"
 
 	"golang.org/x/sync/errgroup"
-	"golang.org/x/sync/semaphore"
 
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
@@ -51,35 +52,95 @@ func LoadStandings(endpoint string) (*api.StandingsResponse, error) {
 	return &parsed, nil
 }
 
-func run(fmtline string, arg ...interface{}) ([]byte, error) {
-	line := fmt.Sprintf(fmtline, arg...)
+type runOption func(*exec.Cmd)
+
+func WithDir(dir string) runOption {
+	return func(c *exec.Cmd) {
+		c.Dir = dir
+	}
+}
+
+func WithStderr(file string) runOption {
+	_ = os.MkdirAll(path.Dir(file), 0755)
+	w, err := os.Create(file)
+	if err != nil {
+		panic(err)
+	}
+	return func(c *exec.Cmd) {
+		c.Stderr = w
+	}
+}
+
+func WithStdout(file string) runOption {
+	_ = os.MkdirAll(path.Dir(file), 0755)
+	w, err := os.Create(file)
+	if err != nil {
+		panic(err)
+	}
+	return func(c *exec.Cmd) {
+		c.Stdout = w
+	}
+}
+
+func Run(fmtline string, opt ...interface{}) (string, error) {
+	args := []interface{}{}
+	opts := []runOption{}
+	for _, arg := range opt {
+		var f runOption
+		if reflect.TypeOf(arg).ConvertibleTo(reflect.TypeOf(f)) {
+			opts = append(opts, arg.(runOption))
+		} else {
+			args = append(args, arg)
+		}
+	}
+	line := fmt.Sprintf(fmtline, args...)
 	log.Debug("Running command", zap.String("cmd", line))
 
 	cmd := exec.Command("bash", "-c", line)
 
-	stderr := bytes.Buffer{}
-	cmd.Stderr = &stderr
+	for _, opt := range opts {
+		opt(cmd)
+	}
 
-	stdout, err := cmd.Output()
+	stderr := bytes.Buffer{}
+	if cmd.Stderr == nil {
+		cmd.Stderr = &stderr
+	} else {
+		cmd.Stderr = io.MultiWriter(cmd.Stderr, &stderr)
+	}
+
+	stdout := bytes.Buffer{}
+	if cmd.Stdout == nil {
+		cmd.Stdout = &stderr
+	} else {
+		cmd.Stdout = io.MultiWriter(cmd.Stdout, &stderr)
+	}
+
+	err := cmd.Run()
 
 	if err != nil {
 		log.Error("Command failed",
 			zap.String("cmd", line),
 			zap.Error(err),
 			zap.ByteString("stderr", stderr.Bytes()),
-			zap.ByteString("stdout", stdout),
+			zap.ByteString("stdout", stdout.Bytes()),
 		)
-		return nil, err
+		return "", err
 	}
 
 	log.Info("Command finished", zap.String("cmd", line))
-	return stdout, nil
+	return stdout.String(), nil
+}
+
+func MustRun(fmtline string, opt ...interface{}) string {
+	return unwrap(Run(fmtline, opt...))
 }
 
 func fetch(url, branch, output string) error {
 	log.Info("Downloading repo", zap.String("url", url), zap.String("branch", branch), zap.String("dest", output))
 
-	_, err := run("git clone %s --branch=%s %s", url, branch, output)
+	_ = os.RemoveAll(output)
+	_, err := Run("git clone %s --branch=%s %s", url, branch, output)
 	if err != nil {
 		log.Error("Failed to download repo",
 			zap.Error(err),
@@ -100,21 +161,21 @@ func FetchSubmit(group, project, task, dest string) error {
 	)
 }
 
-func FetchSubmits(group, endpoint, taskName string) ([]string, error) {
-	standings, err := LoadStandings(endpoint)
+func FetchSubmits(args *Args) ([]string, error) {
+	standings, err := LoadStandings(args.Endpoint)
 	if err != nil {
 		return nil, err
 	}
 
-	s := semaphore.NewWeighted(100)
 	g := errgroup.Group{}
+	g.SetLimit(100)
 	count := 0
 
 	branches := []string{}
 	for _, user := range standings.Standings.Users {
 		for _, grp := range user.Groups {
 			for _, task := range grp.Tasks {
-				if task.Task != taskName {
+				if task.Task != args.TaskName {
 					continue
 				}
 
@@ -131,9 +192,7 @@ func FetchSubmits(group, endpoint, taskName string) ([]string, error) {
 				name := task.Task
 				branches = append(branches, fmt.Sprintf("%s/%s/%s", "solutions", prj, name))
 				g.Go(func() error {
-					s.Acquire(context.TODO(), 1)
-					defer s.Release(1)
-					return FetchSubmit(group, prj, name, "solutions")
+					return FetchSubmit(args.GitlabGroup, prj, name, "solutions")
 				})
 				count += 1
 			}
@@ -149,41 +208,113 @@ func FetchSubmits(group, endpoint, taskName string) ([]string, error) {
 	return branches, nil
 }
 
-func BuildSubmits(group, endpoint, task, target string) error {
-	solutions, err := FetchSubmits(group, endpoint, task)
+func BuildSubmits(args *Args) (map[string]string, error) {
+	solutions, err := FetchSubmits(args)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	err = fetch("git@gitlab.com:danlark/cpp-advanced-hse.git", "main", "repo")
 	if err != nil {
+		return nil, err
+	}
+
+	g := errgroup.Group{}
+	g.SetLimit(10)
+
+	bins := make(map[string]string)
+
+	for _, s := range solutions {
+		solution := s
+		bin := fmt.Sprintf("bins/%s/%s", solution, args.TaskTarget)
+		bins[solution] = bin
+		g.Go(func() error {
+			build := fmt.Sprintf("build/%s", solution)
+			MustRun("mkdir -p %s", build)
+			MustRun("rsync -a repo/ %s", build)
+			MustRun("rsync -a %s/ %s", solution, build)
+			MustRun("rm -rf build", WithDir(build))
+			MustRun("cmake -B build -DCMAKE_BUILD_TYPE=ASAN", WithDir(build))
+			MustRun("cmake --build build --target %s --parallel=4", args.TaskTarget, WithDir(build))
+			MustRun("mkdir -p bins/%s", solution)
+			MustRun("cp %s/build/%s %s", build, args.TaskTarget, bin)
+
+			log.Info("Built solution", zap.String("solution", solution))
+			return nil
+		})
+	}
+
+	err = g.Wait()
+	if err != nil {
+		return nil, err
+	}
+
+	return bins, nil
+}
+
+func RunFuzzing(args *Args) error {
+	bins, err := BuildSubmits(args)
+	if err != nil {
+		log.Error("Failed to build submits", zap.Error(err))
 		return err
 	}
 
-	for _, solution := range solutions {
-		unwrap(run("cd repo && git reset --hard"))
-		unwrap(run("cd repo && git clean -xdf"))
-		unwrap(run("cp -r %s %s", solution, "repo"))
-		unwrap(run("cd repo && rm -rf build"))
-		unwrap(run("cd repo && cmake -B build -DCMAKE_BUILD_TYPE=ASAN"))
-		unwrap(run("cd repo && cmake --build build --parallel --target %s", target))
-		unwrap(run("mkdir -p bins/%s", solution))
-		unwrap(run("cp repo/build/%s bins/%s/%s", target, solution, target))
+	wg := sync.WaitGroup{}
+	wg.Add(len(bins))
 
-		log.Info("Built solution", zap.String("solution", solution))
+	stats := make(map[string]*bool)
+
+	for k, v := range bins {
+		finished := false
+		stats[k] = &finished
+
+		go func(solution string, bin string) {
+			start := time.Now()
+			l := log.With(zap.String("solution", solution))
+			l.Info("Start fuzzing solution")
+			_, err := Run(
+				"%s %s -fork=%d -timeout=600 -max_total_time=%d -reload", bin, args.Corpus, args.Jobs, int(args.Timeout.Seconds()),
+				WithStderr(fmt.Sprintf("logs/%s.err", solution)),
+				WithStdout(fmt.Sprintf("logs/%s.out", solution)),
+			)
+			if err != nil {
+				l.Error("Solution failed", zap.Error(err), zap.Duration("duration", time.Since(start)))
+			} else {
+				finished = true
+				l.Info("Solution finished")
+			}
+			wg.Done()
+		}(k, v)
+	}
+
+	log.Info("Started fuzzing workers", zap.Int("count", len(bins)))
+
+	wg.Wait()
+
+	for solution, ok := range stats {
+		if *ok {
+			fmt.Printf("Solution %s completed successfully!\n", solution)
+		} else {
+			fmt.Printf("Solution %s failed:(\n", solution)
+		}
 	}
 
 	return nil
 }
 
+type Args struct {
+	GitlabGroup string
+	Endpoint    string
+	TaskName    string
+	MainRepo    string
+	TaskTarget  string
+	Corpus      string
+	Timeout     time.Duration
+	Jobs        int
+}
+
 var (
-	args struct {
-		GitlabGroup string
-		Endpoint    string
-		TaskName    string
-		MainRepo    string
-		TaskTarget  string
-	}
+	args Args
 
 	RootCmd = &cobra.Command{
 		Use:   "fuzz",
@@ -194,7 +325,8 @@ var (
 		Use:   "fetch",
 		Short: "Fetch submits",
 		RunE: func(cmd *cobra.Command, _args []string) error {
-			_, err := FetchSubmits(args.GitlabGroup, args.Endpoint, args.TaskName)
+			_, _ = cmd, _args
+			_, err := FetchSubmits(&args)
 			return err
 		},
 	}
@@ -203,7 +335,18 @@ var (
 		Use:   "build",
 		Short: "Build submits",
 		RunE: func(cmd *cobra.Command, _args []string) error {
-			return BuildSubmits(args.GitlabGroup, args.Endpoint, args.TaskName, args.TaskTarget)
+			_, _ = cmd, _args
+			_, err := BuildSubmits(&args)
+			return err
+		},
+	}
+
+	RunCmd = &cobra.Command{
+		Use:   "run",
+		Short: "Fetch, build & run submits",
+		RunE: func(cmd *cobra.Command, _args []string) error {
+			_, _ = cmd, _args
+			return RunFuzzing(&args)
 		},
 	}
 )
@@ -221,9 +364,13 @@ func initCommands() {
 	RootCmd.PersistentFlags().StringVar(&args.GitlabGroup, "gitlab-group", "cpp-advanced-hse-2022", "Gitlab group name")
 	RootCmd.PersistentFlags().StringVar(&args.TaskName, "task", "", "Task name")
 	RootCmd.PersistentFlags().StringVar(&args.TaskTarget, "target", "", "Build target")
+	RootCmd.PersistentFlags().StringVar(&args.Corpus, "corpus", "corpus", "Path to the corpus")
+	RootCmd.PersistentFlags().DurationVar(&args.Timeout, "timeout", time.Hour*24, "Fuzzing timeout")
+	RootCmd.PersistentFlags().IntVar(&args.Jobs, "jobs", 4, "Number of fuzzing jobs per solution")
 
 	RootCmd.AddCommand(FetchCmd)
 	RootCmd.AddCommand(BuildCmd)
+	RootCmd.AddCommand(RunCmd)
 }
 
 func init() {
